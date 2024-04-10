0aa913585ba522d77356a84a4b678ad23c04e0fc
Francisco Carmona <jcarmfran@gmail.com>
2024-04-10 07:31:19 -0500

    Add reqs and setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="disaster-tweet-classification",
+    version="0.0.0",
+    author="cisco",
+    author_email="jcarmfran@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
