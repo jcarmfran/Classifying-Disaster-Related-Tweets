fd94dba9c41bb5d4426eb163fcd7668f235f8b2f
Francisco Carmona <jcarmfran@gmail.com>
2024-04-11 09:15:11 -0500

    Add model training stage components, ml-model, and pipeline

--- disaster/pipeline/train_pipeline.py
+++ disaster/pipeline/train_pipeline.py
@@ -2,15 +2,18 @@ import sys
 from disaster.logger import logging
 from disaster.exception import CustomException
 from disaster.components.data_ingestion import DataIngestion
-from disaster.components.data_transformation import DataTransformation
-from disaster.entity.config_entity import DataIngestionConfig, DataTransformationConfig
-from disaster.entity.artifact_entity import DataIngestionArtifacts, DataTransformationArtifacts
+from disaster.components.data_transformation import DataTransformationArtifacts
+from disaster.components.model_trainer import ModelTrainer
+
+from disaster.entity.config_entity import DataIngestionConfig, DataTransformationConfig, ModelTrainerConfig
+from disaster.entity.artifact_entity import DataIngestionArtifacts, DataTransformationArtifacts, ModelTrainerArtifacts
 
 
 class TrainPipeline:
     def __init__(self):
         self.data_ingestion_config = DataIngestionConfig()
         self.data_transformation_config = DataTransformationConfig()
+        self.model_trainer_config = ModelTrainerConfig()
 
 
     def start_data_ingestion(self) -> DataIngestionArtifacts:
@@ -19,11 +22,14 @@ class TrainPipeline:
         
         try:
             logging.info("Getting the data from GCLoud Storage bucket")
+            
             data_ingestion = DataIngestion(data_ingestion_config = self.data_ingestion_config)
 
             data_ingestion_artifacts = data_ingestion.initiate_data_ingestion()
+            
             logging.info("Obtained training and validation from GCLoud Storage")
             logging.info("Exited the start_data_ingestion method of TrainPipeline class")
+            
             return data_ingestion_artifacts
 
         except Exception as e:
@@ -43,10 +49,29 @@ class TrainPipeline:
             data_transformation_artifacts = data_transformation.initiate_data_transformation()
             
             logging.info("Exited the start_data_transformation method of TrainPipeline class")
+            
             return data_transformation_artifacts
 
         except Exception as e:
             raise CustomException(e, sys) from e
+
+
+    def start_model_trainer(self, data_transformation_artifacts: DataTransformationArtifacts) -> ModelTrainerArtifacts:
+        
+        logging.info("Entered the start_model_trainer method of TrainPipeline class")
+        
+        try:
+            model_trainer = ModelTrainer(data_transformation_artifacts=data_transformation_artifacts,
+                                        model_trainer_config=self.model_trainer_config
+                                        )
+            model_trainer_artifacts = model_trainer.initiate_model_trainer()
+            
+            logging.info("Exited the start_model_trainer method of TrainPipeline class")
+            
+            return model_trainer_artifacts
+
+        except Exception as e:
+            raise CustomException(e, sys)
 
 
     def run_pipeline(self) -> None:
@@ -56,6 +81,8 @@ class TrainPipeline:
         try:
             data_ingestion_artifacts = self.start_data_ingestion()
             data_transformation_artifacts = self.start_data_transformation(data_ingestion_artifacts=data_ingestion_artifacts)
+            model_trainer_artifacts = self.start_model_trainer(data_transformation_artifacts=data_transformation_artifacts)
+            
             logging.info("Exited the run_pipeline method of TrainPipeline class")   
 
         except Exception as e:
